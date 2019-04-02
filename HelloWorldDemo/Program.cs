ebd9f92703a26286f4d6928f83c924dc1fca5dba
LauhardAndreas <49187755+LauhardAndreas@users.noreply.github.com>
2019-04-02 18:49:25 +0200

    HelloWorldDemo

---
+++ HelloWorldDemo/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //docs.microsoft.com
+
+
+            //ShortKeys
+            //WL -> WriteLine
+            //Strg K+ S steht für Surround 
+
+            //Vererbung Fehlerhandling
+            //Exceptionclasse ist die Basisklasse 
+            //Alle anderen Fehlerklassen erben von Exception
+            //seit C#7 kann man in try catch logik packen
+            try
+            {
+                Console.WriteLine($"Hello World from {args[0]}");
+                Console.WriteLine("Do something after error");
+
+            }
+            catch (IndexOutOfRangeException ioure) when (ioure.Message.Contains("index"))
+            {
+
+                Console.WriteLine(ioure.Message);
+            }
+            catch (Exception)
+            {
+               throw new DemoException();
+            }
+        }
+    }
+
+    // internal ist das default verhalten bei klassen
+    public class DemoException : Exception
+    {
+        // überladener constructor
+        public DemoException(string message)
+        {
+
+        }
+         //basis Construktor 
+         //Compiler verlangt einen Construktor
+        public DemoException()
+        {
+
+        }
+    }
+}
