14673ba1c333af4e35d72cbd2b62b355148713cb
LauhardAndreas <49187755+LauhardAndreas@users.noreply.github.com>
2019-04-02 18:55:48 +0200

    Add files via upload

---
+++ PolymorphieDemo/ShowPolymorphie.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolymorphieDemo
+{
+    public class ShowPolymorphie
+    {
+        public int Sum(int a, int b)
+        {
+            return (a + b);
+        }
+        // Methoden Signatur
+        //rückgabetyp name property typen
+        //int Sum(double a, double b)
+        public int Sum(double a, double b, double c = 0)
+        {
+            return (int)(a + b + c);
+        }
+    }
+}
